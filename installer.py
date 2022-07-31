c161bd1765ae72e0118d7476e4dbb1b4924fea70
blackscalare <blackscalare@gmail.com>
2022-07-31 03:15:18 +0200

    Can now install all files in a directory

--- installer.py
+++ installer.py
@@ -1,6 +1,11 @@
 import argparse
 import subprocess
 import os
+import glob
+
+def parse_files(dir):
+    os.chdir(dir)
+    return glob.glob('*.exe')
 
 def install(files):
     for file in files:
@@ -10,11 +15,15 @@ def install(files):
         subprocess.run(['wine', file])
 
 def main():
-    parser = argparse.ArgumentParser(description='Optional app description')
-    parser.add_argument('-f','--files', nargs='+', help='<Required> Set flag', required=True)
+    parser = argparse.ArgumentParser(description='Enter a list of --files or a --dir')
+    parser.add_argument('-f','--files', nargs='+', help='List of files', required=False)
+    parser.add_argument('-d', '--dir', help='Directory', required=False)
     args = parser.parse_args()
-
-    install(args.files)
+    if args.dir:
+        files = parse_files(args.dir)
+        install(files)
+    else:
+        install(args.files)
 
 if __name__ == '__main__':
     main()
